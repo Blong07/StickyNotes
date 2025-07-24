b317ca41786a7741bbec45a96487a0d50ba4a0f3
Alex Blong <alexanderblong07@gmail.com>
2025-07-24 21:14:45 +1200

    Initial Commit

---
+++ StickyNotes/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  StickyNotes
+//
+//  Created by Alexander Blong on 24/07/2025.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ StickyNotesTests/StickyNotesTests.swift
@@ -0,0 +1,17 @@
+//
+//  StickyNotesTests.swift
+//  StickyNotesTests
+//
+//  Created by Alexander Blong on 24/07/2025.
+//
+
+import Testing
+@testable import StickyNotes
+
+struct StickyNotesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
